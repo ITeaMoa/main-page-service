8416148264f9885932e9e313d348b46b2115578f
Jeonsangeon <wjstkddjs28@gmail.com>
2024-12-22 16:47:33 +0900

    refactor.RequestMapping

--- src/main/java/com/iteamoa/mainpage/controller/MainPageController.java
+++ src/main/java/com/iteamoa/mainpage/controller/MainPageController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/main")
 @RequiredArgsConstructor
 public class MainPageController {
 
